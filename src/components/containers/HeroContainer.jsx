ca7c08dff70cbfdfd02c37385389583418e61297
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-12-05 17:46:00 -0300

    Header content added

--- src/components/containers/HeroContainer.jsx
+++ src/components/containers/HeroContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import background from '../../images/heroImages/hero-background.png';
+import Header from './Header';
 import NavBar from './NavBar';
 
 const heroBackground = {
   backgroundImage: `url(${background})`,
-  minHeight: '800px',
+  minHeight: '00px',
   backgroundRepeat: 'no-repeat',
   width: '100%',
   backgroundPositionX: 'right',
@@ -12,8 +13,9 @@ const heroBackground = {
 
 export default function HeroContainer() {
   return (
-    <div className="pt-[57px] pl-[101px]" style={heroBackground}>
+    <div className="pt-[57px] pl-[101px] pr-[94px] pb-[185px]" style={heroBackground}>
       <NavBar />
+      <Header />
     </div>
   );
 }
