aeb231106661917d6e0f862e928da486151cecdb
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-12-08 09:40:06 -0300

    Menu API integration

--- src/components/containers/MenuItemCard.jsx
+++ src/components/containers/MenuItemCard.jsx
@@ -5,13 +5,15 @@ export default function MenuItemCard({ item }) {
     title, description, price, image, categories,
   } = item;
   return (
-    <article className="group w-[330px] pb-[9px] hover:pb-[29px] hover:bg-white rounded-[10px]">
-      <img src={image} alt="Hamburguesa" className="w-[330px] h-[264px] rounded-[10px] object-cover mb-[14px] group-hover:h-[287px]" />
+    <article className="h-[fit-content] group w-[330px] flex flex-col pb-[9px] mb-[70px] hover:pb-[29px] hover:bg-white rounded-[10px] hover:shadow-[0px 16px 24px rgba(56, 81, 114, 0.2)]">
+      <img src={image} alt={title} className="w-[330px] h-[264px] rounded-[10px] object-cover mb-[14px] group-hover:h-[287px]" />
       <p className="font-Syne font-bold text-[22px] leading-[26px] mb-[20px] ml-[20px]">{title}</p>
       <p className="font-Open-Sans font-normal text-[18px] leading-[25px] mb-[40px] ml-[20px]">{description}</p>
-      <div className="flex mx-[20px] justify-between items-center">
-        <p className="font-Open-Sans font-normal text-[18px] leading-[25px] text-[#78909C]">{categories[0].name}</p>
-        <p className="bg-[#FFD600] rounded-[8px] px-[10px] py-[6px] font-Druk-Text-Wide font-bold text-[16px] leading-[16px]">
+      <div className="flex mx-[20px] justify-between items-center justify-self-end mt-auto">
+        {
+          categories.length > 0 && <p className="font-Open-Sans font-normal text-[18px] leading-[25px] text-[#78909C]">{categories[0].name}</p>
+        }
+        <p className="bg-[#FFD600] ml-auto rounded-[8px] px-[10px] py-[6px] font-Druk-Text-Wide font-bold text-[16px] leading-[16px]">
           $
           {price}
           .00
