b41f0913d955dfffdd1a0f50a09ea461004e2a21
Franco Ciro Ferrari <francociroferrari@gmail.com>
2023-01-17 11:48:35 -0300

    NoPage with Strapi

--- src/components/pages/NoPage.jsx
+++ src/components/pages/NoPage.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect } from 'react';
-import ErrorImg from '../../images/emptyStates/error-search.png';
+import React, { useEffect, useState } from 'react';
+import useFetchData from '../../hooks/useFetchData';
+
+const baseURL = process.env.REACT_APP_API_STRAPI;
+const fetchURL = '/api/no-pages/1?populate=*';
 
 export default function NoPage() {
+  // CMS Upgrade
+  const [content, setContent] = useState(null);
+  const { result } = useFetchData(`${baseURL + fetchURL}`);
+
+  useEffect(() => {
+    if (result) {
+      setContent(result.data.attributes);
+    }
+  }, [result]);
+
   useEffect(() => {
     document.title = 'Foodies';
   }, []);
   return (
     <section className="grow flex flex-col justify-center items-center min-h-[89vh]">
-      <img src={ErrorImg} alt="Error search" className="max-w-[216px]" />
-      <p className="font-Syne font-bold text-[20px] leading-[24px] text-[#121212] max-w-[303px] text-center mt-[23px]">¡No hemos encontrado lo que buscas!</p>
+      {
+        content
+        && (
+          <>
+            <img src={baseURL + content.noPageImage.data.attributes.url} alt={content.noPageImageAlt} className="max-w-[216px]" />
+            <p className="font-Syne font-bold text-[20px] leading-[24px] text-[#121212] max-w-[303px] text-center mt-[23px]">{content.text}</p>
+          </>
+        )
+      }
     </section>
   );
 }
